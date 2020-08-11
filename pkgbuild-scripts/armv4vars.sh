be46349091221ac48b0858d3a878d9d173024de3
Adan Scotney <adan.scotney@gmail.com>
2020-08-11 11:01:43 +0100

    New armv4vars.sh

---
+++ pkgbuild-scripts/armv4vars.sh
@@ -0,0 +1,10 @@
+. "${BASH_SOURCE%/*}"/devkitarm.sh
+
+PORTLIBS_PREFIX=${PORTLIBS_ROOT}/armv4
+PATH=$PORTLIBS_PREFIX/bin:$PATH
+
+export CFLAGS="-march=armv4 -O2 -ffunction-sections -fdata-sections"
+export CXXFLAGS="${CFLAGS}"
+export CPPFLAGS="-DARM9 -I${PORTLIBS_PREFIX}/include"
+export LDFLAGS="-L${PORTLIBS_PREFIX}/lib"
+export LIBS=""
